1825e65ab86291ec5de5d14154f5c7eeb63c9150
Arseni Luchinin <Lu4inin23@yandex.ru>
2023-09-25 13:54:28 +0300

    Добавьте файлы проекта.

---
+++ MultiTask.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MultiTask
+{
+    static class Instruments
+    {
+        public static void writeRedText(String outputString)
+        {
+            ConsoleColor fontColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(outputString);
+            Console.ForegroundColor = fontColor;
+        }
+        public static void enterNumber(String innputMessage, out int returnedInt, String ErrorMessage = "Неверный формат записи")
+        {
+            while (true)
+            {
+                Console.WriteLine(innputMessage);
+                if ((Int32.TryParse(Console.ReadLine(), out returnedInt)) && (returnedInt > 0))
+                    break;
+
+                else writeRedText(ErrorMessage);
+            }
+        }
+
+        public static void enterName(String Message,out String name, String ErrorMessage = "Неверный формат записи имени") {
+            while (true)
+            {   
+                Console.WriteLine(Message);
+                name = Console.ReadLine();
+                String pattern = "^[А-ЯЁA-Z][а-яёa-z]{" + (name.Length - 1) + "}";
+                Regex regex = new Regex(pattern);
+                if (regex.IsMatch(name))
+                    break;
+                else
+                    writeRedText(ErrorMessage);
+            }
+        }
+
+        public static void enterWord(String Message, out String word, String ErrorMessage = "Неверный формат записи")
+        {
+            while (true)
+            {
+                Console.WriteLine(Message);
+                word = Console.ReadLine();
+                Regex regex = new Regex(@"\w*", RegexOptions.IgnoreCase);
+                if (regex.IsMatch(word))
+                    break;
+                else
+                    writeRedText(ErrorMessage);
+            }
+        }
+    }
+}   
